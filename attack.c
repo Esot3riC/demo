099920f470d4dd8ba1a704cfcdd3f07b17deb77a
maddy <maddy@kali.example.com>
2020-06-11 23:26:18 -0400

    attack.c

--- attack.c
+++ attack.c
@@ -4,6 +4,6 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-      system("sleep 20");
+      system("/usr/local/bin/score 79b95ac2-29f2-47d9-b5bd-6b418dceb90e");
 }
 
